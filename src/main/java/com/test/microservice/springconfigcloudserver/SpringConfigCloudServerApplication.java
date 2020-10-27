9ccc7a679b4428b7ec77951997a8d0a6ea68bf73
OM <om.mahajan@cropdata.in>
2020-10-27 10:45:42 +0530

    added properties

---
+++ src/main/java/com/test/microservice/springconfigcloudserver/SpringConfigCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.test.microservice.springconfigcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigCloudServerApplication.class, args);
+	}
+
+}
